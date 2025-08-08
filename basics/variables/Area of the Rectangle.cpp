b9d46b522d64753b91d8d0957136fed233e274e7
esai-vel-murugan <esaivelmurugans@gmail.com>
2025-08-09 00:29:15 +0530

    Create Area of the Rectangle.cpp

---
+++ basics/variables/Area of the Rectangle.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int length=6;
+   int width = 4;
+   cout<<"Length: "<<length<<"\n";
+   cout<<"Width: "<<width<<"\n";
+   cout<<"Area of the rectangle: "<<length*width<<"\n";
+}
