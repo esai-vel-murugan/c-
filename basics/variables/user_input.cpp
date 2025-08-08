90c9bf533bc3ee600eb00661cc560b46e42366f1
esai-vel-murugan <esaivelmurugans@gmail.com>
2025-08-09 00:39:12 +0530

    Create user_input.cpp

---
+++ basics/variables/user_input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int x,y,sum;
+ cout<<"Type one number: "<<"\n";
+ cin>>x;
+ cout<<"Type Another Number: "<<"\n";
+ cin>>y;
+ sum=x+y;
+ cout<<"The sum of the two number is: "<<sum<<"\n"; 
+}
