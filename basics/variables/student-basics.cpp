99a4fd5e320104e424a9fadc1367eea59bfd90fb
esai-vel-murugan <esaivelmurugans@gmail.com>
2025-08-09 00:23:20 +0530

    Create student-basics.cpp

---
+++ basics/variables/student-basics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int student_id, student_age;
+   float student_fee;
+   char student_section;
+   
+   student_id=20;
+   student_age=16;
+   student_fee=85.5;
+   student_section='c';
+
+   cout<<"student id: "<<student_id<<"\n";
+   cout<<"student Age: "<<student_age<<"\n";
+   cout<<"student fee: "<<student_fee<<"\n";
+   cout<<"student Section: "<<student_section<<"\n";
+}
